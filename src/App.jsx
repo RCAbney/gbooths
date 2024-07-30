f027140ab0017f7246b6f2d4eb99eff8677fc594
RCAbney <ryan@rcabney.com>
2024-07-29 20:33:13 -0400

    add react query and switch to json promise

--- src/App.jsx
+++ src/App.jsx
@@ -5,58 +5,60 @@ import AllBooths from "./routes/AllBooths";
 import MyBooths from "./routes/MyBooths";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
     const allBooths = useBoothStore((state) => state.booths);
     const setAllBooths = useBoothStore((state) => state.setAllBooths);
 
+    const fetchAllBooths = async () => {
+        const res = await fetch("/data/gencon-2024.json");
+        return res.json();
+    };
+
+    const getBoothsQuery = useQuery({
+        queryKey: ["allBooths"],
+        queryFn: fetchAllBooths,
+    });
+
+    const { data, isError, isLoading } = getBoothsQuery;
+
     useEffect(() => {
-        if (allBooths.length === 0) {
-            async function getData() {
-                const response = await fetch("/data/gencon-2024.csv", {
-                    method: "get",
-                    headers: {
-                        "content-type": "text/csv;charset=UTF-8",
-                    },
-                });
-                const reader = response.body.getReader();
-                const result = await reader.read();
-                const decoder = new TextDecoder("utf-8");
-                const csv = decoder.decode(result.value);
-                const results = Papa.parse(csv, { header: true });
-                const rows = results.data;
-                const keyed = rows.map((row, index) => ({
+        if (data && allBooths.length === 0) {
+            const keyed = data.map((row, index) => {
+                if (typeof row.Location === "number") {
+                    row.Location = row.Location.toString();
+                }
+                return {
                     ...row,
                     rowKey: `${row.BGGId ? row.BGGId : "NoBGGId"}-${index}`,
                     rowLocationNum: row.Location
-                    ? parseInt(row.Location.replace(/\D/g, ""))
+                        ? parseInt(row.Location.replace(/\D/g, ""))
                         : 0,
-                }));
-                const sorted = keyed.sort((a, b) => {
-                    const pubA = a.Publisher?.toUpperCase() || "";
-                    const pubB = b.Publisher?.toUpperCase() || "";
-                    if (pubA < pubB) {
-                        return -1;
-                    }
-                    if (pubA > pubB) {
-                        return 1;
-                    }
-                    return 0;
-                });
-                setAllBooths(sorted);
-            }
-            getData();
+                };
+            }).sort((a, b) => {
+                const pubA = a.Publisher?.toUpperCase() || "";
+                const pubB = b.Publisher?.toUpperCase() || "";
+                if (pubA < pubB) {
+                    return -1;
+                }
+                if (pubA > pubB) {
+                    return 1;
+                }
+                return 0;
+            });
+            setAllBooths(keyed);
         }
-    }, [allBooths.length]);
+    }, [allBooths.length, data]);
 
     return (
         <div className="app">
             <ToastContainer autoClose={750} position="top-center" />
             <Routes>
-              <Route path="/my-booths" element={<MyBooths />} />
-              <Route path="/all-booths" element={<AllBooths />} />
-              <Route path="/" element={<MyBooths />} />
+                <Route path="/my-booths" element={<MyBooths />} />
+                <Route path="/all-booths" element={<AllBooths />} />
+                <Route path="/" element={<MyBooths />} />
             </Routes>
         </div>
     );
